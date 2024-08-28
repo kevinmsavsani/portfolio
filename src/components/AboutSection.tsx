4e4e521d7fa5254ab3687f33182e298f92cb2a05
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-29 00:33:20 +0530

    updated about

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -189,34 +189,37 @@ const AboutSection = () => {
           >
             <h3 className="text-xl text-cyan-500 font-bold pt-4">Skills</h3>
             {config.map((section, index) => (
-              <motion.div key={index}>
+              <div key={index}>
                 <h3 className="text-2xl font-bold">{section.category}</h3>
-                <div className="flex flex-wrap gap-6 m-4">
+                <motion.div className="flex flex-wrap gap-6 m-4">
                   {section.items.map((item, itemIndex) => (
-                    <div
+                    <motion.div
                       key={itemIndex}
                       className="flex flex-col items-center w-16 h-16"
+                      whileHover={{ scale: 1.1 }}
                     >
                       <motion.img
                         initial={{ filter: 'grayscale(100%)' }}
-                        whileHover={{ filter: 'grayscale(0%)', scale: 1.1 }}
+                        whileHover={{ filter: 'grayscale(0%)' }}
                         transition={{ duration: 0.3 }}
                         alt={item.name}
                         src={item.imgSrc}
                         className="w-16 h-16"
                       />
                       <motion.h5
-                        initial={{ opacity: 0, y: 0 }}
-                        whileHover={{ opacity: 1, y: -96 }}
+                        initial={{ opacity: 0, translateY: 0 }}
+                        animate={{ opacity: 0, translateY: 0 }}
+                        whileHover={{ opacity: 1, translateY: -10 }} // Adjusted translateY for a smooth effect
                         transition={{ duration: 0.3 }}
-                        className="bg-white text-black text-center py-2 px-2 text-xs rounded-lg"
+                        className="absolute bg-white text-black text-center py-2 px-2 text-xs rounded-lg"
+                        style={{ top: '0%', transform: 'translateY(-50%)' }} // Positioning the text below the image
                       >
                         {item.name}
                       </motion.h5>
-                    </div>
+                    </motion.div>
                   ))}
-                </div>
-              </motion.div>
+                </motion.div>
+              </div>
             ))}
           </div>
         </div>
