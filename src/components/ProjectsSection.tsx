8bb15303afcd99a1384c47f5dd0d920d43cb5881
kevinmsavsani <kevinmsavsani@gmail.com>
2024-08-29 12:52:55 +0530

    updated project styles

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -11,159 +11,200 @@ import { Autoplay } from 'swiper/modules';
 // import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/autoplay';
-
+import './swiper.css';
 import { Button } from '@headlessui/react';
 
 export const projects = [
   {
-    title: "CoinPlus",
-    description: "Trading platform for cryptocurrencies",
-    technologies: ["React", "CSS", "Redux", "Bootstrap", "TypeScript", "Solidity", "NodeJS", "MongoDB"],
+    title: 'CoinPlus',
+    description: 'Trading platform for cryptocurrencies',
+    technologies: [
+      'React',
+      'CSS',
+      'Redux',
+      'Bootstrap',
+      'TypeScript',
+      'Solidity',
+      'NodeJS',
+      'MongoDB',
+    ],
     img: `./proyecto-13.jpg`,
-    demoLink: "https://coin-plus.vercel.app/",
-    repoLink: "https://github.com/Nahuel61920/CoinPlus",
+    demoLink: 'https://coin-plus.vercel.app/',
+    repoLink: 'https://github.com/Nahuel61920/CoinPlus',
   },
   {
-    title: "Justice",
-    description: "Lawyers website",
-    technologies: ["HTML5", "CSS", "JavaScript", "jQuery", "Bootstrap"],
+    title: 'Justice',
+    description: 'Lawyers website',
+    technologies: ['HTML5', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap'],
     img: `./proyecto-14.jpg`,
-    demoLink: "https://nahuel61920.github.io/Justice/",
-    repoLink: "https://github.com/Nahuel61920/Justice",
+    demoLink: 'https://nahuel61920.github.io/Justice/',
+    repoLink: 'https://github.com/Nahuel61920/Justice',
   },
   {
-    title: "PI Dogs",
-    description: "Individual Project",
-    technologies: ["React", "CSS", "Redux", "NodeJS", "ExpressJS", "Sequelize", "PostgreSQL"],
+    title: 'PI Dogs',
+    description: 'Individual Project',
+    technologies: [
+      'React',
+      'CSS',
+      'Redux',
+      'NodeJS',
+      'ExpressJS',
+      'Sequelize',
+      'PostgreSQL',
+    ],
     img: `./proyecto-15.jpg`,
-    demoLink: "https://pi-dogs-main-ashy.vercel.app/",
-    repoLink: "https://github.com/Nahuel61920/PI-DOGS-MAIN",
+    demoLink: 'https://pi-dogs-main-ashy.vercel.app/',
+    repoLink: 'https://github.com/Nahuel61920/PI-DOGS-MAIN',
   },
   {
-    title: "Power Engine",
-    description: "Automobiles Website",
-    technologies: ["HTML5", "CSS", "JavaScript"],
+    title: 'Power Engine',
+    description: 'Automobiles Website',
+    technologies: ['HTML5', 'CSS', 'JavaScript'],
     img: `./proyecto-7.jpg`,
-    demoLink: "https://nahuel61920.github.io/Power-Engine/",
-    repoLink: "https://github.com/Nahuel61920/Power-Engine",
+    demoLink: 'https://nahuel61920.github.io/Power-Engine/',
+    repoLink: 'https://github.com/Nahuel61920/Power-Engine',
   },
   {
-    title: "Spider-Man fan page",
-    description: "Fan page Spider-Man",
-    technologies: ["HTML5", "CSS", "JavaScript", "Bootstrap"],
+    title: 'Spider-Man fan page',
+    description: 'Fan page Spider-Man',
+    technologies: ['HTML5', 'CSS', 'JavaScript', 'Bootstrap'],
     img: `./proyecto-6.jpg`,
-    demoLink: "https://nahuel61920.github.io/Spider-Man/",
-    repoLink: "https://github.com/Nahuel61920/Spider-Man",
+    demoLink: 'https://nahuel61920.github.io/Spider-Man/',
+    repoLink: 'https://github.com/Nahuel61920/Spider-Man',
   },
   {
-    title: "EveryRunning",
-    description: "Website shoes",
-    technologies: ["HTML5", "CSS", "JavaScript", "Bootstrap"],
+    title: 'EveryRunning',
+    description: 'Website shoes',
+    technologies: ['HTML5', 'CSS', 'JavaScript', 'Bootstrap'],
     img: `./proyecto-12.jpg`,
-    demoLink: "https://nahuel61920.github.io/EveryRunning/",
-    repoLink: "https://github.com/Nahuel61920/EveryRunning",
+    demoLink: 'https://nahuel61920.github.io/EveryRunning/',
+    repoLink: 'https://github.com/Nahuel61920/EveryRunning',
   },
   {
-    title: "Premier Food",
-    description: "Fast food",
-    technologies: ["HTML5", "CSS", "JavaScript", "Sass"],
+    title: 'Premier Food',
+    description: 'Fast food',
+    technologies: ['HTML5', 'CSS', 'JavaScript', 'Sass'],
     img: `./proyecto-5.jpg`,
-    demoLink: "https://nahuel61920.github.io/PremierFood/",
-    repoLink: "https://github.com/Nahuel61920/PremierFood",
+    demoLink: 'https://nahuel61920.github.io/PremierFood/',
+    repoLink: 'https://github.com/Nahuel61920/PremierFood',
   },
   {
-    title: "ShopTec",
-    description: "Ecommerce",
-    technologies: ["HTML5", "CSS", "Sass", "JavaScript"],
+    title: 'ShopTec',
+    description: 'Ecommerce',
+    technologies: ['HTML5', 'CSS', 'Sass', 'JavaScript'],
     img: `./proyecto-8.jpg`,
-    demoLink: "https://nahuel61920.github.io/shop-tec/",
-    repoLink: "https://github.com/Nahuel61920/shop-tec",
+    demoLink: 'https://nahuel61920.github.io/shop-tec/',
+    repoLink: 'https://github.com/Nahuel61920/shop-tec',
   },
   {
-    title: "Elegant Hand",
-    description: "Watch Shop",
-    technologies: ["HTML5", "CSS", "JavaScript"],
+    title: 'Elegant Hand',
+    description: 'Watch Shop',
+    technologies: ['HTML5', 'CSS', 'JavaScript'],
     img: `./proyecto-10.jpg`,
-    demoLink: "https://nahuel61920.github.io/ElegantHand/",
-    repoLink: "https://github.com/Nahuel61920/ElegantHand",
+    demoLink: 'https://nahuel61920.github.io/ElegantHand/',
+    repoLink: 'https://github.com/Nahuel61920/ElegantHand',
   },
   {
-    title: "Vida en papel",
-    description: "Bookstore",
-    technologies: ["HTML5", "CSS", "JavaScript"],
+    title: 'Vida en papel',
+    description: 'Bookstore',
+    technologies: ['HTML5', 'CSS', 'JavaScript'],
     img: `./proyecto-9.jpg`,
-    demoLink: "https://nahuel61920.github.io/vida-en-papel/",
-    repoLink: "https://github.com/Nahuel61920/vida-en-papel",
+    demoLink: 'https://nahuel61920.github.io/vida-en-papel/',
+    repoLink: 'https://github.com/Nahuel61920/vida-en-papel',
   },
   {
-    title: "Plus Anime",
-    description: "Anime website",
-    technologies: ["HTML5", "CSS", "JavaScript"],
+    title: 'Plus Anime',
+    description: 'Anime website',
+    technologies: ['HTML5', 'CSS', 'JavaScript'],
     img: `./proyecto-11.jpg`,
-    demoLink: "https://nahuel61920.github.io/PlusAnime/",
-    repoLink: "https://github.com/Nahuel61920/PlusAnime",
+    demoLink: 'https://nahuel61920.github.io/PlusAnime/',
+    repoLink: 'https://github.com/Nahuel61920/PlusAnime',
   },
 ];
 
 const ProjectsSection: React.FC = () => {
   return (
-        <section id="projects" className='bg-black py-12'>
-            <h2 className="text-white text-4xl font-bold text-center mb-8">
-                <FormattedMessage
-                    id='projects'
-                    defaultMessage='Projects'
-                />
-            </h2>
-            <div className="overflow-x-hidden" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
-                <Swiper
-                    spaceBetween={30}
-                    loop={true}
-                    grabCursor={true}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
-                    modules={[Autoplay]}
-                    breakpoints={{
-                        0: {
-                            slidesPerView: 1,
-                        },
-                        768: {
-                            slidesPerView: 2,
-                        },
-                        1024: {
-                            slidesPerView: 3,
-                        },
-                    }}
-                >
-                    {projects.map((project, index) => (
-                        <SwiperSlide key={index} className='text-center w-80'>
-                            <img
-                                src={project.img}
-                                alt={project.title}
-                                className="w-full opacity-50 transform scale-75 transition-transform duration-300 ease-in-out"
-                            />
-                            <div className="transform scale-0 pt-4 transition-transform duration-300 ease-in-out swiper-slide-active:transform scale-100">
-                                <h3 className="text-2xl text-black">{project.title}</h3>
-                                <p className="text-lg pt-4 text-gray-500">{project.description}</p>
-                                <p className="text-lg pt-4 text-gray-500">
-                                    {project.technologies.join(' - ')}
-                                </p>
-                                <a href={project.demoLink} className="mx-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition" target="_blank" rel="noopener noreferrer"><span>Demo</span></a>
-                                <a href={project.repoLink} className="mx-2 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700 transition" target="_blank" rel="noopener noreferrer">Repository</a>
-                            </div>
-                        </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-            <div className='flex justify-center items-center w-full h-full'>
-                <Link to="/project">
-                    <Button />
-                </Link>
-            </div>
-        </section>
-    )
+    <section id="projects" className="bg-black py-12">
+      <h2 className="text-white text-4xl font-bold text-center mb-8">
+        <FormattedMessage id="projects" defaultMessage="Projects" />
+      </h2>
+      <div
+        className="overflow-x-hidden"
+        data-aos="flip-left"
+        data-aos-easing="ease-out-cubic"
+        data-aos-duration="2000"
+      >
+        <Swiper
+          spaceBetween={30}
+          loop={true}
+          grabCursor={true}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+        >
+          {projects.map((project, index) => (
+            <SwiperSlide key={index} className="text-center w-80">
+              <img
+                src={project.img}
+                alt={project.title}
+                className="w-full opacity-50 transform scale-75 transition-transform duration-300 ease-in-out"
+              />
+              <div className="slide-content transform scale-0 pt-4 transition-transform duration-300 ease-in-out swiper-slide-active:transform flex flex-col gap-4">
+                <div className='flex flex-col gap-1'>
+                  <h3 className="text-2xl text-white font-bold">
+                    {project.title}
+                  </h3>
+                  <p className="text-sm text-gray-500">
+                    {project.description}
+                  </p>
+                </div>
+                <p className="text-sm pt-2 text-gray-500">
+                  {project.technologies.join(' - ')}
+                </p>
+                <div className='flex w-full items-center justify-center gap-2'>
+                  <a
+                    href={project.demoLink}
+                    className="mx-2 px-4 py-2 bg-cyan-500 text-white rounded hover:bg-cyan-700 transition"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span>Demo</span>
+                  </a>
+                  <a
+                    href={project.repoLink}
+                    className="mx-2 px-4 py-2 border-2 border-white text-white rounded transition"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Repository
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      <div className="flex justify-center items-center w-full h-full">
+        <Link to="/project">
+          <Button />
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default ProjectsSection;
